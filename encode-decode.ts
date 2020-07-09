49d5a0f9bf3744bfb277891d88323666678c7fb1
Jeffrey Knight <jeffrey.knight@gmail.com>
2020-07-09 20:12:19 +0000

    Cleanup comments

--- encode-decode.ts
+++ encode-decode.ts
@@ -1,26 +1,35 @@
 import {ethers} from 'ethers';
 
-//Jeffrey Knight <jknight@derivadex.com>
-//Jul 9, 2020 at 4:33:39 PM
+/*
+This script generates authentication headers for the Derivadex REST API
+from a mnemonic
 
-// Mnemonic generator for testing: https://allprivatekeys.com/mnemonic-code-converter#english
+Mnemonic generator for testing:     
+   https://allprivatekeys.com/mnemonic-code-converter#english
 
-// Add your mnemonic here (SECURITY NOTE: use a test mnemonic only!)
+**SECURITY NOTE**
+Testing only - use a generic test mnemonic.
+Do not use your own live mnemonic here
+*/
+
+// Add your mnemonic here 
 const mnemonic = 'inflict stairs vast caution vacant female boring label mandate more cash heart pen school pattern';
 
 // The message to encode
 const message = 'hello';
 
+
 async function encode(message: string, mnemonic: string) {
   const wallet = ethers.Wallet.fromMnemonic(mnemonic);                        
   const signedMessage = await wallet.signMessage(message);                    
-  const address = await wallet.getAddress();                                 
+  const address = await wallet.getAddress(); 
 
-  console.log('--Encode--------------');
-  console.log(`Message: ${message}`);   
-  console.log(`Signed Message: ${signedMessage}`);     
-  console.log(`Wallet Address: ${address}`);  
-  console.log('----------------------');  
+  console.log(`--Headers--------------
+x-auth-ethereum-address: ${address} 
+x-auth-message: ${message}   
+x-auth-signature: ${signedMessage}     
+-----------------------
+`);  
 
   return signedMessage;                            
 };
